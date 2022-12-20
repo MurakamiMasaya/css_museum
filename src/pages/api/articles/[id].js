538330ad7236ff4b364c2e6206eeab34f45e954d
Murakami Masaya <masaya.murakami@arsaga.jp>
2022-12-20 13:24:15 +0900

    async awaitの修正

--- src/pages/api/articles/[id].js
+++ src/pages/api/articles/[id].js
@@ -4,9 +4,12 @@ export default function handler(req, res){
   const { method, query } = req
   try{
     if(method === "GET"){
-      const result = await axios.get(`http://localhost:4030/articles${query.id}`)
-        .then((res) => res.data)
-      return res.status(200).json(result)
+      const getArticle = async() => {
+        const result = await axios.get(`http://localhost:4030/articles${query.id}`)
+          .then((res) => res.data)
+        return res.status(200).json(result)
+      }
+      getArticle()
     }
   } catch {
 
